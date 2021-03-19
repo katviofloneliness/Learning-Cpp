c713c56eb02e91537705f585b67d9f0bddcd0b43
katviofloneliness <80598507+katviofloneliness@users.noreply.github.com>
2021-03-19 17:19:00 +0100

    Add files via upload

---
+++ C++ Learning/Library.cpp
@@ -0,0 +1,14 @@
+//******************************
+// Library 
+//******************************
+// Preprocessor 
+//******************************
+#include <string>
+#include <vector> // reference to library vector
+//******************************
+// Using
+//******************************
+using namespace<std> // using - removing prefixes ex. std::string var; -> string var;
+//******************************
+// Function reference
+//******************************
